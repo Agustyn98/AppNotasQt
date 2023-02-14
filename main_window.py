f833b813e628cd98fa5bb7f3ebc9a0efe0401893
Agus <agustinadaro@hotmail.com>
2023-02-13 21:03:15 -0300

    first commit

---
+++ main_window.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5 import QtWidgets
+
+# Import the converted .ui file
+from ui_main_window import Ui_MainWindow
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
