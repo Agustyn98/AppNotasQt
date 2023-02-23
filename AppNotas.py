d310d05f68c1613e1cb85275104045a7e39428a5
Agus <agustinadaro@hotmail.com>
2023-02-23 00:09:31 -0300

    background of qtextedit, cmmented out for now

--- AppNotas.py
+++ AppNotas.py
@@ -58,6 +58,7 @@ if __name__ == "__main__":
                 "primary>list.inactiveSelectionBackground": "#9D479F",
                 "primary>list.inactiveSelectionBackground": "#783E7A",
                 "input.background": "#202124",
+                #"background>textarea": "202124",
             }
         )
 
